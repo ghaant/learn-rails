0aab22a93470cf66ddeff914c470872cade62d6a
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-04-20 16:51:57 +0200

    Email set for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: Rails.application.credentials[Rails.env.to_sym][:domain_name],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.credentials[Rails.env.to_sym][:email_provider_username],
+    password: Rails.application.credentials[Rails.env.to_sym][:email_provider_password]
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => Rails.application.credentials[Rails.env.to_sym][:domain_name] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
